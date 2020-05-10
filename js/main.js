6f8e9ed800a6d72327b362e3743569fb5829eb67
zpa1986 <59637710+zpa1986@users.noreply.github.com>
2020-05-10 12:02:48 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@
 // в куки записываем значение города и создаем для него дату экспирации +1 день
 function getInfo() {
 	let city = document.getElementById("city").value;
-	document.querySelector('#save').style.display = 'none';
 	if (city!==""){
 	let date = new Date(Date.now() + 86400e3);
 	date = date.toUTCString();
