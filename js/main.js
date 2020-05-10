a1d3921d0e501724fba9e4867c72ff030363f5e4
zpa1986 <59637710+zpa1986@users.noreply.github.com>
2020-05-10 11:33:38 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // при нажатии на кнопку "Сохранить" получаем значение города из поля ввода и создаем куки
 // в куки записываем значение города и создаем для него дату экспирации +1 день
-function getInfo() {
+function saveCity() {
 	let city = document.getElementById("city").value;
 	if (city!==""){
 	let date = new Date(Date.now() + 86400e3);
@@ -8,7 +8,7 @@ function getInfo() {
 	document.cookie =encodeURIComponent('City')+'='+encodeURIComponent(city)+'; expires='+date+'; path=/';
 }
 }
-
+// напрочь убивает куки)))
 function clearCity() {
 	window.location.reload()
 	document.querySelector('#input_city').style.display = '';
@@ -24,30 +24,15 @@ function clearCity() {
 
 }
 
+// function freezeChk(){
+// const mem = [];
+// for (let i=0; i<elements.length; i++) {
+// 	let element = elements[i];
+// 	element.preventDefault()
+// 	}
+// }
 
-// получаем значение куки по ключу заданному ключу (свойству) и возвращаем его
-function getCookie(name) {
-	var matches = document.cookie.match(new RegExp(
-		'(?:^|\s)' + name.replace(/([.$?*+\\\/{}|()\[\]^])/g, '\\$1') + '=(.*?)(?:;|$)'));
-	return matches ? decodeURIComponent(matches[1]) : undefined;
-}
-// получаем значение куки по ключу city и если это значени существует, то
-// 1. убираем поле ввода города
-// 2. делаем видимым поле с приветствием
-// 3. выводим значение города
 
-let nameCity = getCookie('City')
-if (nameCity!==undefined){
-	document.querySelector('#input_city').style.display = 'none';
-	document.querySelector('#greeting').style.display = '';
-	document.querySelector('#greeting_text').innerText="Ваш город: "+nameCity
-}
-
-// создаем пустой массив и обходим все элементы формы с тегом input и типом чекбокс
-// нажатой галке назначаем 1, а если галки нет, то 0. Далее строим массив из запомненных положений
-// также проверяем, нажата ли хоть одна галка. если не нажата, то ничего не делаем
-// а если хоть одна галка нажата, то записываем массив в localStorage в виде JSON
-let elements = document.forms[0].querySelectorAll("input[type='checkbox']")
 function saveChk(){
 const mem = [];
 for (let i=0; i<elements.length; i++) {
@@ -63,6 +48,31 @@ console.log(mem)
 // здесь передаем в локальное хранилище именно JSON, чтобы потом не работать со строкой
 localStorage.setItem("chk", JSON.stringify(mem))
 }
+
+// получаем значение куки по ключу заданному ключу (свойству) и возвращаем его
+function getCookie(name) {
+	var matches = document.cookie.match(new RegExp(
+		'(?:^|\s)' + name.replace(/([.$?*+\\\/{}|()\[\]^])/g, '\\$1') + '=(.*?)(?:;|$)'));
+	return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+// получаем значение куки по ключу city и если это значени существует, то
+// 1. убираем поле ввода города
+// 2. делаем видимым поле с приветствием
+// 3. выводим значение города
+let elements = document.forms[0].querySelectorAll("input[type='checkbox']")
+let nameCity = getCookie('City')
+if (nameCity!==undefined){
+	document.querySelector('#input_city').style.display = 'none';
+	document.querySelector('#greeting').style.display = '';
+	document.querySelector('#greeting_text').innerText="Ваш город: "+nameCity
+}
+
+// создаем пустой массив и обходим все элементы формы с тегом input и типом чекбокс
+// нажатой галке назначаем 1, а если галки нет, то 0. Далее строим массив из запомненных положений
+// также проверяем, нажата ли хоть одна галка. если не нажата, то ничего не делаем
+// а если хоть одна галка нажата, то записываем массив в localStorage в виде JSON
+
+
 
 // заводим в переменную значение из локалсторидж по ключу chk, затем проверяем
 // если в хранилище по этому ключу есть что-то, то получаем массив
